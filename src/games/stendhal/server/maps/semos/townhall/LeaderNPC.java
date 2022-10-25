929313983adb6a2f0f643801d7124430050ea0db
maxwang <maxwang@localhost>
2022-10-25 11:04:29 +0100

    Finished my issues

--- src/games/stendhal/server/maps/semos/townhall/LeaderNPC.java
+++ src/games/stendhal/server/maps/semos/townhall/LeaderNPC.java
@@ -74,8 +74,14 @@ public class LeaderNPC implements ZoneConfigurator {
 					        		if (weapon.getName().endsWith(" hand sword")) {
 					        			// this is a special case, we deal with explicitly
 					        			comment = "I see you use twin swords. They have a superb damage capability but as you cannot wear a shield with them, you will find it harder to defend yourself if attacked.";
-					        		} else if (damage >= 5) {
+					        		} 
+					        		else if (weapon.getName().endsWith("rod of the gm")) {
 					        			comment = "That " + weapon.getName() + " is a powerful weapon, it has a good damage to rate ratio.";
+				        				comment += "The rod of the GM is the weapon with the highest attack in the game!!!";
+				        				}  
+					        		else if (damage >= 5) {
+					        			comment = "That " + weapon.getName() + " is a powerful weapon, it has a good damage to rate ratio.";
+					        			
 					        			if (weapon.getAttackRate() < 3) {
 					        				comment += " Despite the fast rate being useful, the low attack will not help you against strong creatures. Something heavier would be better then.";
 					        			} else if (weapon.getAttackRate() > 6) {
