77a03b61f42159ee6edbcadab8ec93d2098e675f
Bekhruz <bekhruz.suleymanov@student.manchester.ac.uk>
2022-12-03 21:31:50 +0000

    Add tests and start handcart

---
+++ src/games/stendhal/server/entity/mapstuff/handcart/Handcart.java
@@ -0,0 +1,25 @@
+/*************************
+ *                   (C) Copyright 2012-2016 - Stendhal                    *
+ *************************
+ *************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *************************/
+package games.stendhal.server.entity.mapstuff.handcart;
+
+
+/**
+ * Handcart - looks like a chest and acts like one but easy enough to carry around.
+ *
+ * @author Kang Fan and Bekhruz
+ */
+
+
+
+public class Handcart extends Chest implements MovementListener {
+	
+}
